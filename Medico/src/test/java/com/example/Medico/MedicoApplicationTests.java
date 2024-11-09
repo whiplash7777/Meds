cb125effa60fd237fa78bed53b576b50f0e627b3
whiplash7777 <vikaskumarsingh48@gmail.com>
2024-11-09 11:59:36 +0530

    first commit

---
+++ Medico/src/test/java/com/example/Medico/MedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
